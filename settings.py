2b4267fd42fa93eb22f3f71252c738eaf011c694
Anibal Yanez <Anibal@Anibals-MacBook-Pro.local>
2017-11-04 00:31:22 +0900

    Test

--- settings.py
+++ settings.py
@@ -17,7 +17,3 @@ class Settings():
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
-
-        ## Alien settings
-        self.alien_speed_factor = 2
-
